4b4a85a0b176176c1304ad1bf863fd0e84ed7f27
Brandon Pettenati <3brandonjp@gmail.com>
2024-11-09 22:29:09 -0500

    Main process fro generating recycling categories using promptGen.py and ollama

---
+++ catGen.py
@@ -0,0 +1,18 @@
+import sys, os
+from promptGen import genPrompt
+#from keyInterpreter import interpret
+# python -c "from catGen import genCategories; print(genCategories('I need to get rid of sheets of metal'))"
+
+
+response = ""
+#userPrompt = ""
+
+#if __name__ == "__main__":
+
+def genCategories(userPrompt):
+    
+    chatPrompt = genPrompt(userPrompt)
+    #response = interpret(prompt)
+    return chatPrompt
+
+#sys.exit(response)
